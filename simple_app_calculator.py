08fd4b83632964df17d34e47e2d10be19347a9b5
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-05-10 14:52:06 +0800

    Pseudocode

---
+++ simple_app_calculator.py
@@ -0,0 +1,9 @@
+# This program will ask the user to pick which mathematical operations (addition, subtraction, multiplication, and division) to perform then ask for two numbers.
+
+# Ask the user which mathematical operation to perform
+# Ask the user to enter the first number
+# Ask the user to enter the second number
+# Perform the operation
+# Print the result
+# Print an error message when there is an error
+# Ask the user if they want to try again
